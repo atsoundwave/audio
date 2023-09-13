af710a0254d76abc945198a50f6c41c63fbf427c
angelsflyinhell <angelsflyinhell@namespace.media>
2023-09-13 19:27:43 +0200

    feat: connect to database

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,57 @@
 use std::env;
 
-use actix_web::{middleware::Logger, web::Data, App, HttpServer};
+use actix_cors::Cors;
+use actix_web::{http::header, middleware::Logger, web::Data, App, HttpServer};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 mod routes;
 mod utils;
 
+pub struct AppState {
+    db: Pool<Postgres>,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    env::set_var("RUST_LOG", "actix_web=debug,actix_server=info,audio=info");
+
+    dotenv::dotenv().ok();
     env_logger::init();
 
-    HttpServer::new(|| {
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let pool = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+    {
+        Ok(pool) => {
+            log::info!("Connected to database");
+            pool
+        }
+        Err(err) => {
+            log::error!("Failed to connect to the database: {:?}", err);
+            panic!();
+        }
+    };
+
+    HttpServer::new(move || {
         let logger = Logger::default();
 
-        App::new().wrap(logger).service(routes::core::ping::ping)
+        let cors = Cors::default()
+            .allowed_methods(vec!["GET", "POST", "PATCH", "DELETE"])
+            .allowed_headers(vec![
+                header::CONTENT_TYPE,
+                header::AUTHORIZATION,
+                header::ACCEPT,
+            ])
+            .supports_credentials();
+
+        App::new()
+            .app_data(Data::new(AppState { db: pool.clone() }))
+            .wrap(logger)
+            .wrap(cors)
+            .service(routes::core::ping::ping)
     })
     .bind(("0.0.0.0", 80))?
     .run()
