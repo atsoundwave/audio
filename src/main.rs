6112a4c82432a39c34e5ee0711eb13a666139c25
angelsflyinhell <angelsflyinhell@namespace.media>
2023-09-13 17:18:16 +0200

    feat: create test response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use actix_web::{middleware::Logger, App, HttpServer};
+
+mod routes;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        let logger = Logger::default();
+
+        App::new().wrap(logger).service(routes::core::ping::ping)
+    })
+    .bind(("0.0.0.0", 80))?
+    .run()
+    .await
 }
